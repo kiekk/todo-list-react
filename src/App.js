db68ff6cef5a1c7f2a3912300cab96e1e987fd02
shyoon <dbstnsgh2@naver.com>
2021-08-25 23:40:52 +0900

    todoList 생성, map으로 반복

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import { MdAdd, MdCheckBox, MdRemoveCircleOutline } from 'react-icons/md'
 import './style.scss'
 
 function App() {
+  const todoList = [
+    {
+      id: 1,
+      todo: '할일 1',
+      done: false,
+    },
+    {
+      id: 2,
+      todo: '할일 2',
+      done: false,
+    },
+    {
+      id: 3,
+      todo: '할일 3',
+      done: false,
+    },
+    {
+      id: 4,
+      todo: '할일 4',
+      done: false,
+    },
+  ]
+
   return (
     <div className="TodoTemplate">
       <div className="app-title">일정 관리</div>
@@ -14,24 +37,17 @@ function App() {
           </button>
         </form>
         <div className="TodoList">
-          <div className="TodoListItem">
-            <div className="checkbox">
-              <MdCheckBox />
-              <div className="text">할일1</div>
+          {todoList.map((todo) => (
+            <div className="TodoListItem" key={todo.id}>
+              <div className="checkbox">
+                <MdCheckBox />
+                <div className="text">{todo.todo}</div>
+              </div>
+              <div className="remove">
+                <MdRemoveCircleOutline />
+              </div>
             </div>
-            <div className="remove">
-              <MdRemoveCircleOutline />
-            </div>
-          </div>
-          <div className="TodoListItem">
-            <div className="checkbox">
-              <MdCheckBox />
-              <div className="text">할일1</div>
-            </div>
-            <div className="remove">
-              <MdRemoveCircleOutline />
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
